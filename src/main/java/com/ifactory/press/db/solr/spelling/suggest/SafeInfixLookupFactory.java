c3b9d86c5f5ffc60b458775d8a6cc40646aca5b4
Robert Ivey <rivey@oreilly.com>
2017-10-23 21:49:46 -0700

    Update IFPress Plugin to Solr 7.1.0 Compliance
    toPath on line 62
    
    https://oreillymedia.atlassian.net/browse/SPIDR-42

--- src/main/java/com/ifactory/press/db/solr/spelling/suggest/SafeInfixLookupFactory.java
+++ src/main/java/com/ifactory/press/db/solr/spelling/suggest/SafeInfixLookupFactory.java
@@ -2,7 +2,6 @@ package com.ifactory.press.db.solr.spelling.suggest;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.search.suggest.Lookup;
 import org.apache.lucene.search.suggest.analyzing.AnalyzingInfixSuggester;
@@ -15,20 +14,21 @@ import org.apache.solr.schema.FieldType;
 import org.apache.solr.spelling.suggest.fst.AnalyzingInfixLookupFactory;
 
 /**
- * This class is basically a copy-paste of AnalyzingInfixLookupFactory
- * except it builds a SafeInfixSuggester, and exposes the highlight parameter
+ * This class is basically a copy-paste of AnalyzingInfixLookupFactory except it
+ * builds a SafeInfixSuggester, and exposes the highlight parameter
  */
 public class SafeInfixLookupFactory extends AnalyzingInfixLookupFactory {
 
-    /** 
+    /**
      * Default path where the index for the suggester is stored/loaded from
-     * */
+     *
+     */
     private static final String DEFAULT_INDEX_PATH = "analyzingInfixSuggesterIndexDir";
-    
+
     private static final String HIGHLIGHT = "highlight";
-    
+
     private static final boolean DEFAULT_HIGHLIGHT = true;
-    
+
     @Override
     public Lookup create(@SuppressWarnings("rawtypes") NamedList params, SolrCore core) {
         // mandatory parameter
@@ -42,29 +42,28 @@ public class SafeInfixLookupFactory extends AnalyzingInfixLookupFactory {
         }
         Analyzer indexAnalyzer = ft.getIndexAnalyzer();
         Analyzer queryAnalyzer = ft.getQueryAnalyzer();
-  
+
         // optional parameters
-  
-        String indexPath = params.get(INDEX_PATH) != null ?
-                params.get(INDEX_PATH).toString() :
-                        DEFAULT_INDEX_PATH;
-  
+        String indexPath = params.get(INDEX_PATH) != null
+                ? params.get(INDEX_PATH).toString()
+                : DEFAULT_INDEX_PATH;
+
         int minPrefixChars = params.get(MIN_PREFIX_CHARS) != null
                 ? Integer.parseInt(params.get(MIN_PREFIX_CHARS).toString())
-                        : AnalyzingInfixSuggester.DEFAULT_MIN_PREFIX_CHARS;
-                
+                : AnalyzingInfixSuggester.DEFAULT_MIN_PREFIX_CHARS;
+
         Boolean highlight = params.getBooleanArg(HIGHLIGHT);
         if (highlight == null) {
             highlight = DEFAULT_HIGHLIGHT;
         }
 
-         try {
-             return new SafariInfixSuggester(core.getSolrConfig().luceneMatchVersion, 
-                                           FSDirectory.open(new File(indexPath)), indexAnalyzer,
-                                           queryAnalyzer, minPrefixChars, highlight);
-         } catch (IOException e) {
-             throw new SolrException(ErrorCode.SERVER_ERROR, e);
-         }
+        try {
+            return new SafariInfixSuggester(core.getSolrConfig().luceneMatchVersion,
+                    FSDirectory.open(new File(indexPath).toPath()), indexAnalyzer,
+                    queryAnalyzer, minPrefixChars, highlight);
+        } catch (IOException e) {
+            throw new SolrException(ErrorCode.SERVER_ERROR, e);
+        }
     }
 
 }
