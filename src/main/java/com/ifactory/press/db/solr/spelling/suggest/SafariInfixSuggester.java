8ee21460464777ab4c8c6a23183d1ea5f2534a70
Robert Ivey <rivey@oreilly.com>
2017-10-30 20:57:57 -0700

    SPIDR-94 re-work for version 5 fix the super constructor call
    https://oreillymedia.atlassian.net/browse/SPIDR-94

--- src/main/java/com/ifactory/press/db/solr/spelling/suggest/SafariInfixSuggester.java
+++ src/main/java/com/ifactory/press/db/solr/spelling/suggest/SafariInfixSuggester.java
@@ -28,7 +28,7 @@ public class SafariInfixSuggester extends AnalyzingInfixSuggester {
 
     public SafariInfixSuggester(Version matchVersion, Directory dir, Analyzer indexAnalyzer, Analyzer queryAnalyzer,
             int minPrefixChars, boolean highlight) throws IOException {
-        super(matchVersion, dir, indexAnalyzer, queryAnalyzer, minPrefixChars);
+        super(dir, indexAnalyzer, queryAnalyzer, minPrefixChars, true);  // rivey - (matchVersion, take out Needs Testing
         this.highlight = highlight;
 
         showContext = Collections.singleton(new BytesRef(new byte[]{(byte) Context.SHOW.ordinal()}));
