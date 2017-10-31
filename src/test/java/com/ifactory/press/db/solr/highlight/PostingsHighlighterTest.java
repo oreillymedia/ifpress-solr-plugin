8611d57284effc98f3ac59e07d2e94cbd23255b1
Robert Ivey <rivey@oreilly.com>
2017-10-30 21:19:24 -0700

    SPIDR-94 re-work for version 5 add exception catch for ioexception
    https://oreillymedia.atlassian.net/browse/SPIDR-94

--- src/test/java/com/ifactory/press/db/solr/highlight/PostingsHighlighterTest.java
+++ src/test/java/com/ifactory/press/db/solr/highlight/PostingsHighlighterTest.java
@@ -151,6 +151,11 @@ public class PostingsHighlighterTest {
             }
         }
 
+        @Override  // rivey added this
+        protected TokenStreamComponents createComponents(String string) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
     }
 
 }
