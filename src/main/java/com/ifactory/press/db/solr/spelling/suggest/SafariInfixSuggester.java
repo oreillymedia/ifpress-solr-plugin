ac9f6ce68eb6252afefe05c15ff1a8bfc3ad5b78
anthonygroves <agroves@oreilly.com>
2019-10-18 17:13:27 -0400

    Override all lookup methods with safeguard

--- src/main/java/com/ifactory/press/db/solr/spelling/suggest/SafariInfixSuggester.java
+++ src/main/java/com/ifactory/press/db/solr/spelling/suggest/SafariInfixSuggester.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.spell.Dictionary;
 import org.apache.lucene.search.suggest.InputIterator;
 import org.apache.lucene.search.suggest.analyzing.AnalyzingInfixSuggester;
@@ -71,7 +73,8 @@ public class SafariInfixSuggester extends AnalyzingInfixSuggester {
   }
 
   /*
-   * disable highlighting
+      Override each possible lookup method from AnalyzingInfixSuggester to return empty results
+      if suggest build is in progress (instead of throwing error)
    */
   @Override
   public List<LookupResult> lookup(CharSequence key, Set<BytesRef> contexts, boolean onlyMorePopular, int num) throws IOException {
@@ -84,7 +87,71 @@ public class SafariInfixSuggester extends AnalyzingInfixSuggester {
     } else {
       contexts = showContext;
     }
-    return lookup(key, contexts, num, true, highlight);
+    return super.lookup(key, contexts, num, true, highlight);
+  }
+
+  public List<LookupResult> lookup(CharSequence key, int num, boolean allTermsRequired, boolean doHighlight) throws IOException {
+    if (super.searcherMgr == null) {
+      LOG.info("Attempting to retrieve suggestions while suggest build in progress.");
+      return new ArrayList<>();
+    }
+    return super.lookup(key, (BooleanQuery)null, num, allTermsRequired, doHighlight);
+  }
+
+  public List<LookupResult> lookup(CharSequence key, Set<BytesRef> contexts, int num, boolean allTermsRequired, boolean doHighlight) throws IOException {
+    if (super.searcherMgr == null) {
+      LOG.info("Attempting to retrieve suggestions while suggest build in progress.");
+      return new ArrayList<>();
+    }
+    return super.lookup(key, this.toQuery(contexts), num, allTermsRequired, doHighlight);
+  }
+
+  public List<LookupResult> lookup(CharSequence key, Map<BytesRef, BooleanClause.Occur> contextInfo, int num, boolean allTermsRequired, boolean doHighlight) throws IOException {
+    if (super.searcherMgr == null) {
+      LOG.info("Attempting to retrieve suggestions while suggest build in progress.");
+      return new ArrayList<>();
+    }
+    return super.lookup(key, this.toQuery(contextInfo), num, allTermsRequired, doHighlight);
+  }
+
+  public List<LookupResult> lookup(CharSequence key, BooleanQuery contextQuery, int num, boolean allTermsRequired, boolean doHighlight) throws IOException {
+    if (super.searcherMgr == null) {
+      LOG.info("Attempting to retrieve suggestions while suggest build in progress.");
+      return new ArrayList<>();
+    }
+    return super.lookup(key, contextQuery, num, allTermsRequired, doHighlight);
+  }
+
+  private BooleanQuery toQuery(Map<BytesRef, BooleanClause.Occur> contextInfo) {
+    if (contextInfo != null && !contextInfo.isEmpty()) {
+      BooleanQuery.Builder contextFilter = new BooleanQuery.Builder();
+      Iterator var3 = contextInfo.entrySet().iterator();
+
+      while(var3.hasNext()) {
+        Map.Entry<BytesRef, BooleanClause.Occur> entry = (Map.Entry)var3.next();
+        this.addContextToQuery(contextFilter, (BytesRef)entry.getKey(), (BooleanClause.Occur)entry.getValue());
+      }
+
+      return contextFilter.build();
+    } else {
+      return null;
+    }
+  }
+
+  private BooleanQuery toQuery(Set<BytesRef> contextInfo) {
+    if (contextInfo != null && !contextInfo.isEmpty()) {
+      BooleanQuery.Builder contextFilter = new BooleanQuery.Builder();
+      Iterator var3 = contextInfo.iterator();
+
+      while(var3.hasNext()) {
+        BytesRef context = (BytesRef)var3.next();
+        this.addContextToQuery(contextFilter, context, BooleanClause.Occur.SHOULD);
+      }
+
+      return contextFilter.build();
+    } else {
+      return null;
+    }
   }
 
   static class EmptyInputIterator implements InputIterator {
